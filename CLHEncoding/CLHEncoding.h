3027c0e2a90a161be1e431e52d1425413fe36c82
chinthv1 <chinthareddy.vinodreddy@cleanharbors.com>
2020-12-10 22:41:32 +0530

    add framework

---
+++ CLHEncoding/CLHEncoding.h
@@ -0,0 +1,19 @@
+//
+//  CLHEncoding.h
+//  CLHEncoding
+//
+//  Created by VinodReddy on 10/12/20.
+//  Copyright © 2020 Cleanharbors. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CLHEncoding.
+FOUNDATION_EXPORT double CLHEncodingVersionNumber;
+
+//! Project version string for CLHEncoding.
+FOUNDATION_EXPORT const unsigned char CLHEncodingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CLHEncoding/PublicHeader.h>
+
+
---
+++ CLHEncoding/Utility.swift
@@ -0,0 +1,15 @@
+//
+//  Utility.swift
+//  CLHEncoding
+//
+//  Created by VinodReddy on 10/12/20.
+//  Copyright © 2020 Cleanharbors. All rights reserved.
+//
+
+import Foundation
+public struct Utility {
+    public static func encodingMethod() {
+        print("HHi Vinod")
+    }
+    
+}
